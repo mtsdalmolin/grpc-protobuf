cbcd0c2e7a4065848953dd648695a73968c592b9
Matheus Dalmolin da Silva <mtsdalmolin@gmail.com>
2025-08-15 11:01:08 -0300

    add grpc client

--- cmd/grpcClient/main.go
+++ cmd/grpcClient/main.go
@@ -1,34 +1,67 @@
 package main
 
 import (
-  "flag"
-  "log"
+	"context"
+	"flag"
+	"log"
+	"time"
 
+	pb "github.com/mtsdalmolin/grpc-protobuf/internal/pb"
 	"google.golang.org/grpc"
-  "google.golang.org/grpc/credentials/insecure"
-  pb "github.com/mtsdalmolin/grpc-protobuf/internal/pb"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 var (
-	tls                = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
-	caFile             = flag.String("ca_file", "", "The file containing the CA root cert file")
-	serverAddr         = flag.String("addr", "localhost:50051", "The server address in the format of host:port")
-	serverHostOverride = flag.String("server_host_override", "x.test.example.com", "The server name used to verify the hostname returned by the TLS handshake")
+	serverAddr = flag.String("addr", "localhost:50051", "The server address in the format of host:port")
 )
 
-func main() {
-  var opts []grpc.DialOption
+func createCategory(client pb.CategoryServiceClient) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
 
-  opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	response, err := client.CreateCategory(ctx, &pb.CreateCategoryRequest{
+		Name:        "Test Category",
+		Description: "This is a test category",
+	})
+
+	if err != nil {
+		log.Fatalf("could not create category: %v", err)
+	}
+
+	log.Printf("Category created: %v", response)
+}
+
+func main() {
+	flag.Parse()
+
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
 
 	conn, err := grpc.NewClient(*serverAddr, opts...)
 	if err != nil {
 		log.Fatalf("fail to dial: %v", err)
 	}
-  defer conn.Close()
+	defer conn.Close()
 
-  client := pb.NewCategoryServiceClient(conn)
+	client := pb.NewCategoryServiceClient(conn)
 
-  log.Println(client.ListCategories(nil, nil))
+	// createCategory(client)
 
+	// Create a context with timeout
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	// Call ListCategories with proper parameters
+	response, err := client.ListCategories(ctx, &pb.Blank{})
+	if err != nil {
+		log.Fatalf("could not list categories: %v", err)
+	}
+
+	log.Printf("Categories received: %v", response)
+
+	// Print each category
+	for i, category := range response.Categories {
+		log.Printf("Category %d: ID=%s, Name=%s, Description=%s",
+			i+1, category.Id, category.Name, category.Description)
+	}
 }
